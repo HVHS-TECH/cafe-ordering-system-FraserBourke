50e0edaf416b779185ba0e3297908c82cedeb53f
FraserBourke <24347fb@hvhs.school.nz>
2026-06-15 23:33:21 +0000

    Jun 15, 2026, 11:33 PM

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello world!")
+
+//variables
+let Username = "Fraser";
+let money = 50;
+halfMoney = money / 2;
+
+const OUTPUT = document .getElementById("spaceForJavaScriptOutput");
+
+
+
+OUTPUT.innerHTML += ("<p>you have " + money + " dollars </p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " dollars </p>")
