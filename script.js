ccd165d01a4ac86bb1b6af0d0d3840506c3e746d
FraserBourke <24347fb@hvhs.school.nz>
2026-07-20 23:56:54 +0000

    Jul 20, 2026, 11:56 PM

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function addItem() {
     "<p>Order added successfully!</p>";
 
     OUTPUT.innerHTML += " Added to order: "  +  name  + "  has ordered,  " + item + " for $"   + moneySpent ;
-    OUTPUT.innerHTML += "<br>";
+    OUTPUT.innerHTML += " Heres ";
 
 
     }
